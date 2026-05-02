d39e58a6dadd2c5feaa310e98e1282d070795215
Nathan Kimpan <nathankimpan@gmail.com>
2026-05-01 20:16:14 -0400

    done

--- js/controls.js
+++ js/controls.js
@@ -5,6 +5,10 @@ var a = false;
 var d = false;
 var w = false;
 var s = false;
+var ArrowLeft = false;
+var ArrowRight = false;
+var ArrowUp = false;
+var ArrowDown = false;
 
 document.addEventListener("keydown", press);
 document.addEventListener("keyup", release);
@@ -12,45 +16,76 @@ document.addEventListener("keyup", release);
 
 function press(e)
 {
+    // First player controls (WASD)
+    
+    if (e.keyCode == 87) { // W
+        w = true;
+    }
+    if (e.keyCode == 83) { // S
+        s = true;
+    }
     //console.log("Pressed "+ e.keycode);
-    /*if(e.keyCode == 65) 
-        {
-            a=true;
-        }
-        if (e.keyCode == 68)
-        {
-            d=true
-        } */
+    // if (e.keyCode == 65) { // A
+    //     a = true;
+    // }
+    // if (e.keyCode == 68) { // D
+    //     d = true;
+    // }
+
+    // Second player controls (Arrow keys)
+   
+    if (e.keyCode == 38) { // Up arrow
+        ArrowUp = true;
+    }
+    if (e.keyCode == 40) { // Down arrow
+        ArrowDown = true;
+    }
     //console.log("Pressed "+ e.keycode);
-    if(e.keyCode == 87) 
-        {
-            s=true;
-        }
-        if (e.keyCode == 83)
-        {
-            w=true
-        }
+    // if (e.keyCode == 37) { // Left arrow
+    //     ArrowLeft = true;
+    // }
+    // if (e.keyCode == 39) { // Right arrow
+    //     ArrowRight = true;
+    // }
+    
 }
 function release(e)
 {
-        //console.log("Pressed "+ e.keycode);
-        if(e.keyCode == 65) 
-        {
-            a=false;
-        }
-        if (e.keyCode == 68)
-        {
-            d=false;
-        }
-        //console.log("Pressed "+ e.keycode);
-        if(e.keyCode == 87) 
-        {
-            s=false;
-        }
-        if (e.keyCode == 83)
-        {
-            w=false;
-        }
+    // First player controls (WASD)
+    if (e.keyCode == 87) { // W
+        w = false;
+    }
+    if (e.keyCode == 83) { // S
+        s = false;
+    }
+    //console.log("Pressed "+ e.keycode);
+    // if (e.keyCode == 65) { // A
+    //     a = false;
+    // }
+    // if (e.keyCode == 68) { // D
+    //     d = false;
+    // }
+
+    // Second player controls (Arrow keys)
+    
+    if (e.keyCode == 38) { // Up arrow
+        ArrowUp = false;
+    }
+    if (e.keyCode == 40) { // Down arrow
+        ArrowDown = false;
+    }
+    //console.log("Pressed "+ e.keycode);
+    // if (e.keyCode == 37) { // Left arrow
+    //     ArrowLeft = false;
+    // }
+    // if (e.keyCode == 39) { // Right arrow
+    //     ArrowRight = false;
+    // }
+    
+   
 }
+
+
+
 
 
--- js/gameloop.js
+++ js/gameloop.js
@@ -8,15 +8,19 @@ var interval = 1000/60
 canvas = document.getElementById("canvas");
 context = canvas.getContext("2d");
 
-player = new GameObject(100,canvas.height/2,5,100,"#6200ff");
+player = new GameObject(50,canvas.height/2,5,100,"#ff0000");
 player.vx=0;
 player.vy=0;
+
+player2 = new GameObject(950,canvas.height/2,5,100,"#00ff00");
+player2.vx=0;
+player2.vy=0;
 
 npc0 = new GameObject(canvas.width/2,canvas.height/2,100,100,"#6200ff");
 npc0.vx=-4;
 npc0.vy=0;
 
-npc1 = new GameObject(300,canvas.height/2,100,100,"#00ff00")
+npc1 = new GameObject(300,canvas.height/2,100,100,"#00ddff")
 npc2 = new GameObject(600,canvas.height/2,100,100,"#0000ff")
 npc3 = new GameObject(900,canvas.height/2,100,100,"#FF00ff")
 
@@ -35,13 +39,30 @@ function animate()
     {
         player.x -= 4
     }
-    if (w)
+    if (s)
     {
         player.y += 8
     }
-    if (s)
+    if (w)
     {
         player.y -= 8
+    }
+
+    if (ArrowRight)
+    {
+        player2.x += 4
+    }
+    if (ArrowLeft)
+    {
+        player2.x -= 4
+    }
+    if (ArrowDown)
+    {
+        player2.y += 8
+    }
+    if (ArrowUp)
+    {
+        player2.y -= 8
     }
 
     player.move();
@@ -69,6 +90,32 @@ function animate()
             //player.color = "#ff0000" 
             player.y = player.height/2
         }
+
+        player2.move();
+        if (player2.x > canvas.width + player2.width/2)
+        {
+            //player2.vx=-4
+            //player2.color = "#ffff00"
+            player2.x = -player2.width/2
+        }
+        else if (player2.x < -player2.width/2)
+        {
+            //player2.vx=4
+            //player2.color = "#0000ff"
+            player2.x = canvas.width + player2.width/2
+        }
+        if (player2.y > canvas.height - player2.height/2)
+        {
+            //player2.vy=-4
+            //player2.color = "#00ff00"
+            player2.y = canvas.height -player2.height/2
+        }
+        else if (player2.y < player2.height/2)
+        {
+            //player2.vy=4
+            //player2.color = "#ff0000" 
+            player2.y = player2.height/2
+        }
         
        // npc0 collision
        npc0.move();
@@ -91,10 +138,23 @@ function animate()
             npc0.x = player.right() + npc0.width/2;
             npc0.color = "#ff8800";
         }
-        else if (npc0.x > canvas.width - npc0.width/2)
+        else if (npc0.collisionCheck(player2))
         {
-            npc0.vx = -npc0.vx;
-            npc0.x = canvas.width - npc0.width/2;
+            if(npc0.y < player2.y - player2.height/6)
+            {
+                npc0.vx = -Math.abs(npc0.vx);
+                npc0.vy = -Math.abs(npc0.vx);
+            }
+            else if(npc0.y > player2.y + player2.height/6)
+            {
+                npc0.vx = -Math.abs(npc0.vx);
+                npc0.vy = Math.abs(npc0.vx);
+            }
+            else
+            {
+                npc0.vx = -npc0.vx;
+            }
+            npc0.x = player2.left() - npc0.width/2;
             npc0.color = "#6200ff";
         }
         else if (npc0.y < npc0.height/2)
@@ -111,9 +171,16 @@ function animate()
         {
             npc0.x = canvas.width/2;
             npc0.y = canvas.height/2;
-            npc0.vx = -4;
             npc0.vy = 0;
-            npc0.color = "#6200ff";
+            player.y = canvas.height/2;
+        }
+        else if (npc0.x > canvas.width + npc0.width/2)
+        {
+            npc0.x = -canvas.width/2;
+            npc0.y = canvas.height/2;
+            npc0.vy = 0;
+            player2.y = canvas.height/2;
+
         }
         
        // NPC1 collision
@@ -142,13 +209,11 @@ function animate()
         }
 
     player.drawRect();
+    player2.drawRect();
     npc0.drawCircle();
     //npc1.drawCircle();  
     //npc2.drawCircle();  
     //npc3.drawRect();  
-    //player.drawRegistrationPoint();
-    //npc0.drawRegistrationPoint();
-
 
 
 }
